77c78987ae722569305f921987e6d81343893688
Jeffrey Tan <tanhoetheng@gmail.com>
2019-06-05 03:22:54 +0800

    New File

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+	echo "<!DOCTYPE html>";
+	echo "<html lang=\"en\" style='height:100%'>";
+		echo "<head>";
+			include("style/index.html");
+		echo "</head>";
+		echo "<body style='height:100%'>";
+			include("nav.php");		
+			echo "<main class='bd-masthead' id='bgs-img'>";
+				echo "<div class='container'>";
+					echo "<div class='row align-items-center'>";
+						echo "<div class='col-12 mx-auto col-md-12' style='	margin-top:80px'>";
+							echo "<h1 class='mb-3 bd-text-purple-bright text-center'>LET US HELP YOU CRAFT YOUR FUTURE!</h1>";
+							echo "<br /><br />";
+						echo "</div>";
+					echo "</div>";
+					echo "<div class='row align-items-center'>";
+						echo "<div class='col-md-6 order-md-1 text-center text-md-left pr-md-5'>";
+							echo "<p class='lead'>";
+								echo "Enabling our students to thrive in college life and BEYOND!";
+							echo "</p>";	
+						echo "</div>";
+					echo "</div>";
+				echo "</div>";
+			echo "</main>";
+			echo "<div class='container-fluid'>";
+	
+			echo "</div>";
+
+		echo "</body>";
+	echo "</html>";
+?>
---
+++ nav.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-sm bg-light navbar-light fixed-top">
+	<div class="container">
+		<!--Logo-->
+		<a class="navbar-brand" href="#">GODs</a>
+		<!--Links-->
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Home</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="#">Colleges</a>
+			</li>
+		</ul>
+
+		<ul class="navbar-nav ml-auto">	
+			<li class="nav-item">
+				<form class="form-inline" action="/action_page.php">
+			    	<input class="form-control mr-sm-2" type="text" placeholder="Search">
+			    	<button class="btn btn-success" type="submit">Search</button>
+			  	</form>
+		  	</li>
+		  	<li class="nav-item px-2">
+				<a class="nav-link" href="#"><i class="fas fa-sign-in-alt"></i></a>
+			</li>
+			<li class="nav-item px-2">
+				<a class="nav-link" href="#"><i class="fas fa-sign-out-alt"></i></a>
+			</li>
+		</ul>
+	</div>
+</nav>
