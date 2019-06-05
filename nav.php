196cd7787a750cedf90da4de876da65aa573a573
Jeffrey Tan <tanhoetheng@gmail.com>
2019-06-05 22:53:23 +0800

    Using mdn

--- nav.php
+++ nav.php
@@ -1,31 +1,51 @@
-<nav class="navbar navbar-expand-sm bg-light navbar-light fixed-top">
-	<div class="container">
-		<!--Logo-->
-		<a class="navbar-brand" href="#">GODs</a>
-		<!--Links-->
-		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link" href="#">Home</a>
-			</li>
+ <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
 
-			<li class="nav-item">
-				<a class="nav-link" href="#">Colleges</a>
-			</li>
-		</ul>
+            <div class="container">
 
-		<ul class="navbar-nav ml-auto">	
-			<li class="nav-item">
-				<form class="form-inline" action="/action_page.php">
-			    	<input class="form-control mr-sm-2" type="text" placeholder="Search">
-			    	<button class="btn btn-success" type="submit">Search</button>
-			  	</form>
-		  	</li>
-		  	<li class="nav-item px-2">
-				<a class="nav-link" href="#"><i class="fas fa-sign-in-alt"></i></a>
-			</li>
-			<li class="nav-item px-2">
-				<a class="nav-link" href="#"><i class="fas fa-sign-out-alt"></i></a>
-			</li>
-		</ul>
-	</div>
-</nav>
+                <!-- Navbar brand -->
+                <a class="navbar-brand" href="#">GODs</a>
+
+                <!-- Collapse button -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+                    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- Collapsible content -->
+                <div class="collapse navbar-collapse" id="basicExampleNav">
+
+                    <!-- Links -->
+                    <ul class="navbar-nav mr-auto smooth-scroll">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Colleges</a>
+                        </li>
+                    </ul>
+                    <!-- Links -->
+
+                    <!-- Social Icon  -->
+                    <ul class="navbar-nav nav-flex-icons">
+                        <li class="nav-item">
+                            <a class="nav-link">
+                            	<form class="form-inline" action="/action_page.php">
+				    				<input class="form-control mr-sm-2" type="text" placeholder="Search">
+				    				<button class="btn btn-success" type="submit">Search</button>
+			  					</form>
+			  				</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link"><i class="fas fa-sign-in-alt fa-2x mt-3"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link"><i class="fas fa-sign-out-alt fa-2x mt-3"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Collapsible content -->
+
+            </div>
+
+        </nav>
+        <!--/.Navbar-->
