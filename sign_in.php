98ad7a32d60a8705bb6035f375fde6eefae60d38
Jeffrey Tan <tanhoetheng@gmail.com>
2019-06-07 01:02:41 +0800

    add code into code.txt

--- sign_in.php
+++ sign_in.php
@@ -6,53 +6,46 @@
 		echo "</head>";
 		echo "<body>";
 		echo "<div id='intro' class='view' width='100%'>";
-?>
-		<div class='mask rgba-black-strong'>
-			<div class='mt-5 container d-flex align-items-center justify-content-center'>
-				<div class='col-md-5'>
-					
-					<div class ='jumbotron card card-image signin-jumbotron my-0'>
-						<h2 class='display-4 text-white text-center'><strong>GODs</strong></h2>
-					</div>
-					<div class='bg-light'>
-						<form class='px-5 py-5'>
-							<div class="md-form">
-								<i class="fa fa-envelope prefix indigo-text"></i>
-								<input mdbActive class='form-control pl-2' type="email" name='email' required autocomplete="off" placeholder='Email'/>
-							</div>
+			echo"<div class='mask rgba-black-strong'>";
+				echo"<div class='container d-flex align-items-center justify-content-center'>";
+					echo"<div class='mt-3 pt-5 col-md-5'>";
+						echo"<div class ='jumbotron card card-image signin-jumbotron my-0'>";
+							echo"<h2 class='display-4 text-white text-center'><strong>GODs</strong></h2>";
+						echo"</div>";
 
-
-							<div class="md-form">
-								<i class="fas fa-key prefix indigo-text"></i>
-								<input mdbActive class='form-control pl-2' type="password" name='pwd' required autocomplete="off" placeholder='Password'/>
-							</div>
-
-							<div class="d-flex justify-content-around">
-        						<div>
-            						<div class="custom-control custom-checkbox">
-                						<input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
-                						<label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
-        							</div>
-        						</div>
-	        					<div>
-	            					<a href="">Forgot password?</a>
-	        					</div>
-    						</div>
-
-    						<div class="text-center">
-								<button type="submit" class="text-white btn blue-gradient col-md-6 my-4">Sign In</button>
-								<p>
-									Not a member?
-	        						<a href="">Register</a>
-	    						</p>
-	    					</div>
-						</form>
-					</div>
-					
-				</div>
-			</div>
-		</div>
-<?php
+						echo"<div class='bg-light'>";
+							echo"<form class='px-5 py-5'>";
+								echo"<div class='md-form'>";
+									echo"<i class='fa fa-envelope prefix indigo-text'></i>";
+									echo"<input mdbActive class='form-control pl-2' type='email' name='email' required autocomplete='off' placeholder='Email'/>";
+								echo"</div>";
+								echo"<div class='md-form'>";
+									echo"<i class='fas fa-key prefix indigo-text'></i>";
+									echo"<input mdbActive class='form-control pl-2' type='password' name='pwd' required autocomplete='off' placeholder='Password'/>";
+								echo"</div>";
+								echo"<div class='d-flex justify-content-around'>";
+									echo"<div>";
+										echo"<div class='custom-control custom-checkbox'>";
+											echo"<input type='checkbox' class='custom-control-input' id='defaultLoginFormRemember'>";
+											echo"<label class='custom-control-label' for='defaultLoginFormRemember'>Remember me</label>";
+										echo"</div>";
+									echo"</div>";
+									echo"<div>";
+										echo"<a href=''>Forgot password?</a>";
+									echo"</div>";
+								echo"</div>";
+								echo"<div class='text-center'>";
+									echo"<button type='submit' class='text-white btn blue-gradient col-md-6 my-4'>Sign In</button>";
+									echo"<p>";
+										echo"Not a member?";
+										echo"<a href=''>Register</a>";
+									echo"</p>";
+								echo"</div>";
+							echo"</form>";
+						echo"</div>";
+					echo"</div>";
+				echo"</div>";
+			echo"</div>";
 		echo "</div>";
 		echo "</body>";
 	echo "</html>";
