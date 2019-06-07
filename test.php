c86445947c52167a7cd609903254465c21a57a86
Jeffrey Tan <tanhoetheng@gmail.com>
2019-06-07 16:03:11 +0800

    Sign in and register

---
+++ test.php
@@ -0,0 +1,50 @@
+<?php
+	echo "<!DOCTYPE html>";
+	echo "<html lang='en'>";
+		echo "<head>";
+		include("header.html");
+		echo "</head>";
+		echo "<body>";
+		echo "<div id='intro' class='view''>";
+			echo"<div class='mask rgba-black-strong'>";
+				echo"<div class='container d-flex align-items-center justify-content-center'>";
+					echo"<div class='mt-3 pt-5 pb-5 col-md-5'>";
+						echo"<div class ='jumbotron card card-image signin-jumbotron my-0'>";
+							echo"<h2 class='display-4 text-white text-center'><strong>GODs</strong></h2>";
+						echo"</div>";
+						echo "<div class='scrollbar bg-light' id='style-1' style='height:50vh'>";
+							echo"<form class='px-5'>";					
+								echo"<div class='md-form'>";
+									echo"<i class='fas fa-user prefix indigo-text'></i>";
+									echo"<input mdbActive class='form-control pl-2' type='text' name='username' required autocomplete='off' placeholder='Username'/>";
+								echo"</div>";
+								echo"<div class='md-form'>";
+									echo"<i class='fa fa-envelope prefix indigo-text'></i>";
+									echo"<input mdbActive class='form-control pl-2' type='email' name='email' required autocomplete='off' placeholder='Email'/>";
+								echo"</div>";
+								echo"<div class='md-form'>";
+									echo"<i class='fas fa-key prefix indigo-text'></i>";
+									echo"<input mdbActive class='form-control pl-2' type='password' name='pwd' required autocomplete='off' placeholder='Password'/>";
+								echo"</div>";
+								echo"<div class='md-form'>";
+									echo"<i class='fas fa-key prefix indigo-text'></i>";
+									echo"<input mdbActive class='form-control pl-2' type='password' name='retype_pwd' required autocomplete='off' placeholder='Retype Password'/>";
+								echo"</div>";
+								echo "<div class='md-form'>";
+									echo "<i class='fas fa-calendar-day prefix indigo-text'></i>";
+									echo "<input type='text'class='form-control pl-2' name='date_of_birth' onfocus=\"(this.type='date')\" onblur=\"(this.type='text')\"required autocomplete='off' placeholder='Date Of Birth' />";
+								echo "</div>";
+
+								echo"<div class='text-center'>";
+									echo"<button type='submit' class='text-white btn blue-gradient col-md-6 my-4'>Register</button>";
+								echo"</div>";
+							echo"</form>";
+	
+						echo"</div>";
+					echo"</div>";
+				echo"</div>";
+			echo"</div>";
+		echo "</div>";
+		echo "</body>";
+	echo "</html>";
+?>
